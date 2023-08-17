9931c2e3fe9ab6e6186bfae6c3c689f844564741
Roman Shevyakov <roman@mgstr.ru>
2023-08-17 12:30:54 +0600

    feat: change apiRoot builder logic

--- src/service/api/client.ts
+++ src/service/api/client.ts
@@ -1,13 +1,16 @@
-import { ctpClient } from './BuildClient';
-import { ApiRoot, createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { UserAuthOptions } from '@commercetools/sdk-client-v2';
+import { createApiRoot } from './BuildClient';
+import { ApiRoot } from '@commercetools/platform-sdk';
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 
-// Create apiRoot from the imported ClientBuilder and include your Project key
-const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey: process.env.CTP_PROJECT_KEY || '{projectKey}',
-});
+const userCredentials: UserAuthOptions = { username: 'test@t.ru', password: 'test' };
 
-// Example function to check API params
-export const getCustomers = async () => {
-  const customersList = await apiRoot.customers().get().execute();
-  return customersList;
+export let apiRoot: ByProjectKeyRequestBuilder = createApiRoot();
+
+export const turnOnPasswordMode = (userCredentials: UserAuthOptions) => {
+  apiRoot = createApiRoot(userCredentials);
+};
+
+export const turnOffPasswordMode = () => {
+  apiRoot = createApiRoot();
 };
