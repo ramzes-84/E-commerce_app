9fc7dcc4efb8d26446729359834ccc7650377994
Roman Shevyakov <roman@mgstr.ru>
2023-08-18 14:38:43 +0600

    fix: remove sensitive info

--- src/service/api/client.ts
+++ src/service/api/client.ts
@@ -1,9 +1,6 @@
 import { UserAuthOptions } from '@commercetools/sdk-client-v2';
 import { createApiRoot } from './BuildClient';
-import { ApiRoot } from '@commercetools/platform-sdk';
 import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
-
-const userCredentials: UserAuthOptions = { username: 'test@t.ru', password: 'test' };
 
 export let apiRoot: ByProjectKeyRequestBuilder = createApiRoot();
 
