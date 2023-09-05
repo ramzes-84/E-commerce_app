fe6c622ae45ba867d57c74e985202fe603f15f8e
Lyubov Agulova <lu7623@gmail.com>
2023-09-05 12:48:04 +0300

    fix: minor fix

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -26,7 +26,7 @@ export default async function Page({
   return (
     <>
       <CatalogNavPanel category={cat} products={products} />
-      <FiltersApplied searchParams={filters} />
+      <FiltersApplied />
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 justify-start mx-3">
         {products.length !== 0 ? (
           list.map((p) => <CatalogCard key={p.name} product={p} />)
