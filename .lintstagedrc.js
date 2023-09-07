882995e40f187470a3ac4e6aef9d955584f541c4
Kseniya <pen.cil@mail.ru>
2023-09-08 01:56:33 +0500

    fix: test

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
-
-module.exports = {
-  '*.{js,ts,tsx}': [buildEslintCommand, 'npm run format:fix'],
-};
