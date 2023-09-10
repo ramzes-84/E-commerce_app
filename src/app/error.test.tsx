35776bd036fcbdaf512d8c943a71adaf6bc46c92
Lyubov Agulova <lu7623@gmail.com>
2023-09-10 14:55:26 +0300

    test: commit for broken test

--- src/app/error.test.tsx
+++ src/app/error.test.tsx
@@ -6,7 +6,7 @@ describe('Error page', () => {
   it('renders a correct Error page', () => {
     render(<ErrorPage error={new Error()} reset={() => {}} />);
 
-    const btn = screen.getByText('Try again');
+    const btn = screen.getByText('Try 0000again');
     fireEvent.click(btn);
 
     expect(btn).toBeInTheDocument();
