40eeb1ad98325b50ae3bf42ac75ccfca710a0cb2
Roman Shevyakov <roman@mgstr.ru>
2023-09-12 21:33:51 +0600

    feat: change product qty in cart

--- src/app/basket/components/DrawListItems.tsx
+++ src/app/basket/components/DrawListItems.tsx
@@ -2,6 +2,7 @@ import { ButtonRemoveFromCart } from '@/app/product/[key]/components/ButtonRemov
 import { LineItem } from '@commercetools/platform-sdk';
 import Link from 'next/link';
 import Image from 'next/image';
+import AddToCartBtn from '@/app/catalog/components/addToCartBtn';
 
 export function DrawListItems({ lineItems }: { lineItems: LineItem[] }) {
   const liArr = lineItems.map((item) => {
@@ -16,7 +17,7 @@ export function DrawListItems({ lineItems }: { lineItems: LineItem[] }) {
         <Link href={link} className="grow dark:text-blue-600 hover:underline">
           <div>{item.name['en-US']}</div>
         </Link>
-        <div>{item.quantity} pcs.</div>
+        <AddToCartBtn inCart={item.quantity} itemId={item.productId} />
         <div> *</div>
         <div>{item.price.value.centAmount / 100} USD</div>
         <div> =</div>
