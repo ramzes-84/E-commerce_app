d86f98efeeb6b1cbee117bde47391a50b6d5d3b8
Lyubov Agulova <lu7623@gmail.com>
2023-09-14 16:58:26 +0300

    fix: person card size fix

--- src/app/about/components/personCard.tsx
+++ src/app/about/components/personCard.tsx
@@ -13,8 +13,8 @@ export default function PersonCard({ name, role, photoUrl, githubUrl, onClick }:
   return (
     <div className=" hover:-translate-y-1">
       <Border title={name}>
-        <div className="flex items-center gap-x-3.5 m-5 cursor-pointer font-serif" onClick={onClick}>
-          <div className="overflow-hidden w-24 sm:h-24 rounded-[50%] min-[320px]:h-20">
+        <div className="flex items-center gap-x-3.5 m-2 sm:m-5 cursor-pointer font-serif" onClick={onClick}>
+          <div className="overflow-hidden w-24 sm:h-24 rounded-[50%] min-[320px]:h-20 min-[320px]:w-20">
             <Image width={100} height={100} className="inline-block" src={photoUrl} alt="photo" />
           </div>
           <div>
