f4b518b5e2f6bdcba27674b4bf5a0183595dee56
Lyubov Agulova <lu7623@gmail.com>
2023-09-17 14:02:33 +0300

    feat: add info, some styles fixed

--- src/app/about/components/personCard.tsx
+++ src/app/about/components/personCard.tsx
@@ -13,15 +13,15 @@ export default function PersonCard({ name, role, photoUrl, githubUrl, onClick }:
   return (
     <div className=" hover:-translate-y-1">
       <Border title={name}>
-        <div className="flex items-center gap-x-3.5 m-2 sm:m-5 cursor-pointer font-serif" onClick={onClick}>
-          <div className="overflow-hidden w-24 sm:h-24 rounded-[50%] min-[320px]:h-20 min-[320px]:w-20">
+        <div className="flex items-center gap-x-3.5 mt-5 cursor-pointer font-serif" onClick={onClick}>
+          <div className="overflow-hidden w-18 h-18 sm:w-24 sm:h-24 rounded-full ">
             <Image width={100} height={100} className="inline-block" src={photoUrl} alt="photo" />
           </div>
           <div>
-            <span className=" text-lg text-emerald-800 font-bold">{role}</span>
+            <span className=" sm:text-lg text-emerald-800 font-bold md:text-xl">{role}</span>
             <div className="flex flex-wrap items-center gap-1">
               <span>Welcome to my GitHub</span>
-              <a href={githubUrl} target="_blank">
+              <a href={githubUrl} target="_blank" className="w-8 h-8 sm:w-10 sm:h-10">
                 <Image width={45} height={45} className="inline-block" src="/icons8-github.svg" alt="GitHubLogo" />
               </a>
             </div>
